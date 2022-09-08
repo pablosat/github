e8b86b3c09edb026ac2d745ed19a7c9752741fb2
pablosat <pablo@teu.ai>
2022-09-08 16:25:17 -0400

    use ref on mount

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export const Input = ({ onSubmit }) => {
   const [inputValue, setInputValue] = useState("");
+  const ref = useRef(null);
+
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
 
   return (
     <input
+      ref={ref}
       placeholder="Search github user..."
       className="input"
       value={inputValue}
